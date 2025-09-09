e607700fc1e86855733660140914f92a6c2cee2e
Omer Salim Mohmand <omersalim.tech@gmail.com>
2025-09-09 09:24:13 -0600

    init mobile practice

---
+++ practice.js
@@ -0,0 +1,14 @@
+// console.log("In the name of Allah");
+
+const car = {
+  make: "Toyota",
+  model: "Corrola",
+  "Fuel Type": "Petrol",
+  drive: () => {
+    console.log('car is in driving')
+  }
+}
+
+console.log(car.make);
+console.log(car.model);
+car.drive()
