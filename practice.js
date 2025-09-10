ab1bec04f88b46b122b5a35c4e502178570a687e
omersalim.tech@gmail.com <omersalim.tech@gmail.com>
2025-09-09 22:32:38 -0600

    update practice.js

--- practice.js
+++ practice.js
@@ -4,11 +4,29 @@ const car = {
   make: "Toyota",
   model: "Corrola",
   "Fuel Type": "Petrol",
-  drive: () => {
-    console.log('car is in driving')
+  drive: function(){
+    console.log(`${this.make} ${this.model} is driving`)
+  },
+  
+  show: function(){
+    console.log(this)
   }
 }
 
-console.log(car.make);
-console.log(car.model);
-car.drive()
+// console.log(car.make);
+// console.log(car.model);
+// console.log(car['Fuel Type']);
+
+// car.year = "1993";
+// delete car.model;
+
+// car.drive()
+// car.show()
+
+// for(let key in car){
+//   console.log(`${key}: ${car[key]}`)
+// }
+
+console.log(Object.keys(car))
+console.log(Object.values(car))
+console.log(Object.entries(car))
