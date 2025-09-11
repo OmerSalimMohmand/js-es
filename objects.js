d848872ae438103d8ac764122c5b1365537f8899
omersalim.tech@gmail.com <omersalim.tech@gmail.com>
2025-09-10 22:37:18 -0600

    objects final practice

---
+++ objects.js
@@ -0,0 +1,92 @@
+console.log("In the name of Allah");
+
+const car = {
+ make: "Toyota",
+ model: "Corrola",
+"Fuel Type": "Petrol",
+ drive: function() {
+   console.log(`${this.make} ${this.model} is driving`)
+ },
+ show: function() {
+   console.log(this)
+ },
+ color: {
+   "inside color": "black",
+   "outside color": "white",
+ }
+}
+
+console.log(car.make);
+console.log(car.model);
+console.log(car['Fuel Type']);
+
+car.year = "1993";
+delete car.model;
+
+car.drive()
+car.show()
+
+for(let key in car){
+  console.log(`${key}: ${car[key]}`)
+}
+
+console.log(Object.keys(car))
+console.log(Object.values(car))
+console.log(Object.entries(car))
+
+console.log(car['color']['inside color'])
+
+for (let key in car['color']) {
+  console.log(`${key}: ${car['color'][key]}`)
+}
+
+for (let k in car) {
+ for (let e in car[k]){
+    console.log(car[k][e])
+  }
+}
+
+for (let key in car) {
+ if (typeof car[key] === 'object' && car[key] !== null) {
+   for (let inKey in car[key]) {
+     console.log(`${inKey}: ${car[key][inKey]}`);
+   }
+ } else {
+   console.log(`${key}: ${car[key]}`);
+ }
+}
+
+const car = new Object()
+car.brand = "Toyota"
+car.engine = {'fuel': 'Petrol', 'power': 'V6'}
+
+console.log(Object.entries(car))
+
+function Car(make, fuel, power){
+  this.make = make
+  this.engine = {
+    'fuel': fuel,
+    'power': power
+  }
+}
+
+const car = new Car('Toyota', 'Petrol', 'V6');
+
+class Car{
+  constructor(make, fuel, power){
+    this.make = make
+    this.engine = {
+      'fuel': fuel,
+      'power': 'V6'
+    }
+  }
+  
+  print(){
+    console.log(this)
+  }
+}
+
+const car = new Car('Toyota', 'Petrol', 'V6')
+
+console.log(car)
+car.print()
